17098acddc0eeadb50bcf1011cec54106ad68e7d
six-two <git@six-two.dev>
2026-06-01 19:12:26 +0200

    ci: Migrate to properdocs and materialx

--- build.sh
+++ build.sh
@@ -3,11 +3,15 @@
 # Change into the project root
 cd -- "$( dirname -- "${BASH_SOURCE[0]}" )"
 
-# If you created a virtual python environment, source it
-if [[ -f venv/bin/activate ]]; then
-    echo "[*] Using virtual python environment"
-    source venv/bin/activate
+MKDOCS=properdocs
+
+# Even Vercel needs venvs now, since otherwise pip will not work
+if [[ ! -f venv/bin/activate ]]; then
+    echo "[*] Creating venv"
+    python3 -m venv venv
 fi
+echo "[*] Using venv"
+source venv/bin/activate
 
 echo "[*] Installing dependencies"
 python3 -m pip install -r requirements.txt
@@ -26,24 +30,23 @@ cp redirect.html public/index.html
 
 build_with_theme() {
     echo "[*] Building with theme $1"
-    sed "/^site_url:/s|\$|/$1/|" mkdocs.yml > "mkdocs-$1.yml"
-    python3 -m mkdocs build -f "mkdocs-$1.yml" -t "$1" -d public/"$1"
+    sed "/^site_url:/s|\$|/$1/|" $MKDOCS.yml > "$MKDOCS-$1.yml"
+    python3 -m $MKDOCS build -f "$MKDOCS-$1.yml" -t "$1" -d public/"$1"
 }
 
 build_with_theme_no_directory_urls() {
     echo "[*] Building with theme $1"
-    sed -e "/^site_url:/s|\$|/$1/|" -e 's/use_directory_urls: true/use_directory_urls: false/' mkdocs.yml > "mkdocs-$1-no-dir.yml"
-    python3 -m mkdocs build -f "mkdocs-$1-no-dir.yml" -t "$1" -d public/"$1"-no-dir
+    sed -e "/^site_url:/s|\$|/$1/|" -e 's/use_directory_urls: true/use_directory_urls: false/' $MKDOCS.yml > "$MKDOCS-$1-no-dir.yml"
+    python3 -m $MKDOCS build -f "$MKDOCS-$1-no-dir.yml" -t "$1" -d public/"$1"-no-dir
 }
-
 
 build_with_theme mkdocs
 build_with_theme readthedocs
-build_with_theme material
+build_with_theme materialx
 
 build_with_theme_no_directory_urls mkdocs
 build_with_theme_no_directory_urls readthedocs
-build_with_theme_no_directory_urls material
+build_with_theme_no_directory_urls materialx
 
 if [[ -z "$1" ]]; then
     echo "[*] To view the site run:"
